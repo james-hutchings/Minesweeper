2133840f6763ff62348095281153065f7ebac3d1
james-hutchings <u1165847@umail.utah.edu>
2026-08-07 14:33:02 -0600

    Refactored MineSweeperPanelJava to handle any board size and cleaned up first click checks.

--- src/mineSweeper/MineSweeperPanel.java
+++ src/mineSweeper/MineSweeperPanel.java
@@ -5,400 +5,229 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Random;
 
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+
+/*
+ * The panel that contains the Minesweeper game grid.
+*/
 public class MineSweeperPanel extends JPanel implements ActionListener, MouseListener {
-	
-	MineSweeperButton[][] buttonList = new MineSweeperButton[10][10];
-	boolean isClicked = false;
-	int bombCount;
-	
-	public MineSweeperPanel() {
-		this.setLayout(new GridLayout(10, 10));
-		
-		for (int i = 0; i < buttonList.length; i++) {
-			for (int j = 0; j < buttonList[0].length; j++) {
-				MineSweeperButton gameButton = new MineSweeperButton(false, 0, i, j);
-				gameButton.addActionListener(this);
-				gameButton.addMouseListener(this);
-				buttonList[i][j] = gameButton;
-				this.add(gameButton);
-		
-			}
-			
-		}
-		
-	}
-	
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		if (SwingUtilities.isRightMouseButton(e)) {
-			MineSweeperButton button = (MineSweeperButton) e.getSource();
-			if (button.isFlagged) {
-				button.eraseIcon();
-			}
-			
-			else 
-				button.setFlagIamge();
-		}
-		
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		MineSweeperButton button = (MineSweeperButton) e.getSource();
-		int startX = button.getXLocation();
-		int startY = button.getYLocation();
-//		System.out.println("StartX is: " + startX + " And startY is: " + startY);
-		
-		// Checks if player has started game.
-		if (isClicked == false) {
-
-			buttonList[startX][startY].setEnabled(false);
-			isClicked = true;
-			
-			
-			// Handles Edge Cases.
-			// Top left 4 squares.
-			if (startX < 2) {
-				if (startY < 2) {
-					for (int i = 0; i < buttonList.length / 2; i++) {
-						for (int j = 0; j < buttonList[0].length / 2; j++) {
-							buttonList[i][j].setEnabled(false);
-						}
-					}
-					placeMines();
-					
-
-				}
-				
-				// Top Right Four Squares
-				else if (startY >= 8) {
-					for (int i = 0; i < buttonList.length / 2; i++) {
-						for (int j = buttonList[0].length / 2; j < buttonList[0].length; j++) {
-							buttonList[i][j].setEnabled(false);
-						}
-					}
-					placeMines();
-				}
-				
-				// Top Center 6 Squares
-				else {
-					for (int i = 0; i < buttonList.length / 2; i++) {
-						for (int j = startY - 2; j < startY + 3; j++) {
-							buttonList[i][j].setEnabled(false);
-						}
-					}
-					placeMines();
-				}
-
-			}
-
-			// Bottom Left 4 Squares
-			else if (startX >= 8) {
-				if (startY < 2) {
-					for (int i = buttonList.length / 2; i < buttonList.length; i++) {
-						for (int j = 0; j < buttonList[0].length / 2; j++) {
-							buttonList[i][j].setEnabled(false);
-						}
-					}
-					placeMines();
-				}
-				
-				// Bottom Right 4 Squares
-				else if (startY > 7) {
-					for (int i = buttonList.length / 2; i < buttonList.length; i++) {
-						for (int j = buttonList[0].length / 2; j < buttonList[0].length; j++) {
-							buttonList[i][j].setEnabled(false);
-						}
-					}
-					placeMines();
-				}
-				
-				
-				// Bottom Center Squares
-				else {
-					for (int i = buttonList.length / 2; i < buttonList.length; i++) {
-						for (int j = startY - 2; j < startY + 3; j++) {
-							buttonList[i][j].setEnabled(false);
-						}
-					}
-					placeMines();
-				}
-			}
-			
-			// Left Center Squares
-			else if (startX >= 2 && startY <= 1) {
-				for (int i = startX - 2; i < startX + 3; i++) {
-					for (int j = 0; j < buttonList[0].length / 2; j++) {
-						buttonList[i][j].setEnabled(false);
-					}
-				}
-				placeMines();
-				
-			}
-			
-			
-			// Right Center Squares
-			else if (startX <= 8 && startY == 8) {
-				for (int i = startX - 2; i < startX + 3; i++) {
-					for (int j = buttonList[0].length / 2; j < buttonList[0].length; j++) {
-						buttonList[i][j].setEnabled(false);
-					}
-				}
-				placeMines();
-				
-			}
-			
-			
-			
-			// Handles all other cases. 
-			else {
-				for (int i = startX - 2; i < startX + 3; i++) {
-					for (int j = startY - 2; j < startY + 3; j++) {
-						buttonList[i][j].setEnabled(false);
-					}
-				}
-				placeMines();
-			}
-			
-			checkNearBombs();
-			buttonList[0][4].revealBombsNear();
-			buttonList[1][4].revealBombsNear();
-			buttonList[2][4].revealBombsNear();
-			buttonList[3][4].revealBombsNear();
-			buttonList[4][4].revealBombsNear();
-			
-			buttonList[4][0].revealBombsNear();
-			buttonList[4][1].revealBombsNear();
-			buttonList[4][2].revealBombsNear();
-			buttonList[4][3].revealBombsNear();
-			buttonList[4][4].revealBombsNear();
-			
-				
-				}
-		
-		// Handles normal game case where button is not a bomb.
-		else {
-			if (!buttonList[startX][startY].isBomb) {
-				buttonList[startX][startY].revealBombsNear();
-			}
-			
-			// Handles game case where button is a bomb.
-			else {
-				buttonList[startX][startY].setBombImage();
-				for (int i = 0; i < buttonList.length; i++) {
-					for (int j = 0; j < buttonList[0].length; j++) {
-						if (buttonList[i][j].isBomb) {
-							buttonList[i][j].setBombImage();
-							
-						}
-					}
-				}
-				
-				JOptionPane.showMessageDialog(this, "You hit a bomb! The game will now reset.");
-				resetMineSweeper();
-				
-			}
-		}
-				
-			}
-
-	public void placeMines() {
-		int maxBomb = (int) ((buttonList.length * buttonList[0].length) * .25);
-		int bombAmount = 0;
-		Random random = new Random();
-		
-		while (bombAmount < maxBomb) {
-			int randomX = random.nextInt(0,10);
-			int randomY = random.nextInt(0,10);
-			
-			if (buttonList[randomX][randomY].isEnabled() && !buttonList[randomX][randomY].isBomb) {
-				buttonList[randomX][randomY].setBomb();
-				bombAmount++;
-			}
-		}
-	}
-	
-	public void checkNearBombs() {
-
-				for (int i = 0; i < buttonList.length; i++) {
-					for (int j = 0; j < buttonList[0].length; j++) {
-						int bombsNear = 0;
-						if (!buttonList[i][j].isBomb) {
-							
-							
-							// Top left.
-							if (i == 0) {
-								if (j == 0) {
-									for (int x = 0; x < 2; x++) {
-										for (int y = 0; y < 2; y++) {
-											if (buttonList[x][y].isBomb) {
-												bombsNear++;
-											}
-											
-										}
-									}
-									buttonList[i][j].setBombsNear(bombsNear);
-									bombsNear = 0;
-									
-								}
-								
-								// Top Center
-								else if (j < 9) {
-									for (int x = 0; x < 2; x++) {
-										for (int y = j - 1; y < j + 2; y++) {
-											if (buttonList[x][y].isBomb) {
-												bombsNear++;
-											}
-										}
-									}
-									buttonList[i][j].setBombsNear(bombsNear);
-									bombsNear = 0;
-									
-								}
-								
-								// Top Right
-								else if (j == 9) {
-									for (int x = 0; x < 2; x++) {
-										for (int y = 8; y < 10; y++) {
-											if (buttonList[x][y].isBomb) {
-												bombsNear++;
-											}
-										}
-									}
-									
-									buttonList[i][j].setBombsNear(bombsNear);
-									bombsNear = 0;
-								}
-								
-								
-							}
-							
-							// Left Center
-							else if (j == 0) {
-								if (i < 9) {
-									for (int x = i - 1; x < i + 2; x++) {
-										for (int y = 0; y < 2; y++) {
-											if (buttonList[x][y].isBomb) {
-												bombsNear++;
-											}
-										}
-									}
-									buttonList[i][j].setBombsNear(bombsNear);
-									bombsNear = 0;
-								}
-
-								// Bottom Left
-								else if (i == 9) {
-									for (int x = 8; x < 10; x++) {
-										for (int y = 0; y < 2; y++) {
-											if (buttonList[x][y].isBomb) {
-												bombsNear++;
-											}
-											
-										}
-									}
-									buttonList[i][j].setBombsNear(bombsNear);
-									bombsNear = 0;
-									
-								}
-								
-								
-							}
-							
-							// Bottom Center
-							else if (i == 9) {
-								if (j < 9) {
-									for (int x = 8; x < 10; x++) {
-										for (int y = j - 1; y < j + 2; y++) {
-											if (buttonList[x][y].isBomb) {
-												bombsNear++;
-											}
-										}
-									}
-									buttonList[i][j].setBombsNear(bombsNear);
-									bombsNear = 0;
-								}
-
-								// Bottom Right
-								else if (j == 9) {
-									for (int x = 8; x < 10; x++) {
-										for (int y = 8; y < 10; y++) {
-											if (buttonList[x][y].isBomb) {
-												bombsNear++;
-											}
-											
-										}
-									}
-									buttonList[i][j].setBombsNear(bombsNear);
-									bombsNear = 0;
-									
-								}
-								
-								
-							}
-							
-							// Right Center
-							else if (j == 9) {
-								if (i < 9) {
-									for (int x = i - 1; x < i + 2; x++) {
-										for (int y = 8; y < 10; y++) {
-											if (buttonList[x][y].isBomb) {
-												bombsNear++;
-											}
-										}
-									}
-									buttonList[i][j].setBombsNear(bombsNear);
-									bombsNear = 0;
-								}
-							}
-							
-							// All other Cases.
-							else {
-								for (int x = i - 1; x < i + 2; x++) {
-									for (int y = j - 1; y < j + 2; y++) {
-										if (buttonList[x][y].isBomb) {
-											bombsNear++;
-										}
-									}
-								}
-								buttonList[i][j].setBombsNear(bombsNear);
-								bombsNear = 0;
-								
-							}
-							
-							
-						}
-			}
-		}
-		
-	}
-
-
-	public void resetMineSweeper() {
-		for (int i = 0; i < buttonList.length; i++) {
-			for (int j = 0; j < buttonList[0].length; j++) {
-				buttonList[i][j].eraseButton();
-				isClicked = false;
-				
-			}
-		}	
-		
-		
-	}
-	
-
-	
 	private static final long serialVersionUID = 1L;
+
+	private final int rows;
+	private final int cols;
+	private final double bombPercent;
+
+	private final MineSweeperButton[][] buttons;
+	private boolean firstClick = true;
+
+	public MineSweeperPanel(int rows, int cols) {
+		this(rows, cols, 0.25);
+	}
+
+	public MineSweeperPanel(int rows, int cols, double bombPercent) {
+		this.rows = rows;
+		this.cols = cols;
+		this.bombPercent = bombPercent;
+		this.buttons = new MineSweeperButton[rows][cols];
+
+		setLayout(new GridLayout(rows, cols));
+		initBoard();
+	}
+
+	// Initializes the game board with buttons.
+	private void initBoard() {
+		removeAll();
+
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < cols; c++) {
+				MineSweeperButton button = new MineSweeperButton(r, c);
+				button.addActionListener(this);
+				button.addMouseListener(this);
+				buttons[r][c] = button;
+				add(button);
+			}
+		}
+
+
+		revalidate();
+		repaint();
+	}
+
+	// Handles the action when a button is clicked.
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        MineSweeperButton button = (MineSweeperButton) e.getSource();
+
+        if (button.isFlagged() || button.isRevealed()) {
+            return;
+        }
+
+        int row = button.getRow();
+        int col = button.getCol();
+
+        if (firstClick) {
+            placeMines(row, col);
+            calculateAllBombCounts();
+            firstClick = false;
+        }
+
+        if (button.isBomb()) {
+            revealAllBombs();
+            JOptionPane.showMessageDialog(this, "You hit a bomb! The game will now reset.");
+            resetGame();
+        } else {
+            revealCell(row, col);
+        }
+    }
+
+	// Handles right-click to toggle flag on a button.
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (SwingUtilities.isRightMouseButton(e)) {
+            MineSweeperButton button = (MineSweeperButton) e.getSource();
+            button.toggleFlag();
+        }
+    }
+
+	// Places mines on the board, ensuring the first clicked cell and its neighbors are safe
+    private void placeMines(int safeRow, int safeCol) {
+        int totalCells = rows * cols;
+        int desiredBombs = (int) Math.round(totalCells * bombPercent);
+
+        int safeCells = 0;
+        for (int r = safeRow - 1; r <= safeRow + 1; r++) {
+            for (int c = safeCol - 1; c <= safeCol + 1; c++) {
+                if (inBounds(r, c)) {
+                    safeCells++;
+                }
+            }
+        }
+
+        int maxBombs = Math.min(desiredBombs, totalCells - safeCells);
+        int bombsPlaced = 0;
+        Random random = new Random();
+
+        while (bombsPlaced < maxBombs) {
+            int r = random.nextInt(rows);
+            int c = random.nextInt(cols);
+
+            if (buttons[r][c].isBomb()) {
+                continue;
+            }
+
+            if (isInSafeZone(r, c, safeRow, safeCol)) {
+                continue;
+            }
+
+            buttons[r][c].setBomb();
+            bombsPlaced++;
+        }
+    }
+
+    private boolean isInSafeZone(int row, int col, int safeRow, int safeCol) {
+        return Math.abs(row - safeRow) <= 1 && Math.abs(col - safeCol) <= 1;
+    }
+
+    private void calculateAllBombCounts() {
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (!buttons[r][c].isBomb()) {
+                    buttons[r][c].setBombsNear(countAdjacentBombs(r, c));
+                }
+            }
+        }
+    }
+
+    private int countAdjacentBombs(int row, int col) {
+        int count = 0;
+
+        for (int r = row - 1; r <= row + 1; r++) {
+            for (int c = col - 1; c <= col + 1; c++) {
+                if (r == row && c == col) {
+                    continue;
+                }
+                if (inBounds(r, c) && buttons[r][c].isBomb()) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+	// Reveals the cell and, if it has no adjacent bombs, reveals its neighbors recursively.
+    private void revealCell(int startRow, int startCol) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[] { startRow, startCol });
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.remove();
+            int row = current[0];
+            int col = current[1];
+
+            if (!inBounds(row, col)) {
+                continue;
+            }
+
+            MineSweeperButton button = buttons[row][col];
+
+            if (button.isRevealed() || button.isFlagged() || button.isBomb()) {
+                continue;
+            }
+
+            button.revealNumber();
+
+            if (button.getBombsNear() == 0) {
+                for (int r = row - 1; r <= row + 1; r++) {
+                    for (int c = col - 1; c <= col + 1; c++) {
+                        if (inBounds(r, c)) {
+                            MineSweeperButton neighbor = buttons[r][c];
+                            if (!neighbor.isRevealed() && !neighbor.isFlagged() && !neighbor.isBomb()) {
+                                queue.add(new int[] { r, c });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+	// Reveals all bombs on the board, typically called when the player hits a bomb. 
+    private void revealAllBombs() {
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (buttons[r][c].isBomb()) {
+                    buttons[r][c].revealBomb();
+                }
+            }
+        }
+    }
+
+    private void resetGame() {
+        firstClick = true;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                buttons[r][c].resetButton();
+            }
+        }
+    }
+
+    private boolean inBounds(int row, int col) {
+        return row >= 0 && row < rows && col >= 0 && col < cols;
+    }
 
 	public void mousePressed(MouseEvent e) {}
 	public void mouseReleased(MouseEvent e) {}
 	public void mouseEntered(MouseEvent e) {}
 	public void mouseExited(MouseEvent e) {}
+	
 
 }
