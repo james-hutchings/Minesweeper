7df0a3af52c4a0984dfcf2f954697faa5b7ba068
james-hutchings <u1165847@umail.utah.edu>
2026-08-07 14:12:59 -0600

    Refactoed MineSweeperButton to be simpler and easier to read.

--- src/mineSweeper/MineSweeperButton.java
+++ src/mineSweeper/MineSweeperButton.java
@@ -2,91 +2,124 @@ package mineSweeper;
 
 import java.awt.Font;
 import java.awt.Image;
+import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
+/**
+ * A button in the Minesweeper game.
+ */
 public class MineSweeperButton extends JButton {
-
-	boolean isBomb = false;
-	int bombsNear;
-	int xLocation;
-	int yLocation;
-	
-	boolean isFlagged = false;
-	
-	ImageIcon bomb = new ImageIcon("bomb.jpg");
-	Image newBomb = bomb.getImage().getScaledInstance(85,  85, Image.SCALE_SMOOTH);
-	ImageIcon scaledBomb = new ImageIcon(newBomb);
-	
-	ImageIcon flag = new ImageIcon("Flag.png");
-	Image newFlag = flag.getImage().getScaledInstance(85,  85, Image.SCALE_SMOOTH);
-	ImageIcon scaledFlag = new ImageIcon(newFlag);
-	
-	public MineSweeperButton(boolean isBomb, int bombsNear, int xLocation, int yLocation) {
-		this.isBomb = isBomb;
-		this.bombsNear = bombsNear;
-		this.xLocation = xLocation;
-		this.yLocation = yLocation;
-	}
-	
-	
-	public void setXLocation (int x) {
-		this.xLocation = x;
-	}
-	
-	public void setYLocation (int y) {
-		this.yLocation = y;
-	}
-	
-	public int getXLocation () {
-		return xLocation;
-	}
-	
-	public int getYLocation () {
-		return yLocation;
-	}
-	
-	public void setBomb() {
-		this.isBomb = true;
-	}
-	
-	public void setBombsNear(int n) {
-		this.bombsNear = n;
-	}
-
-	public void setBombImage() {
-		this.setIcon(scaledBomb);
-		this.isBomb = true;
-	}
-	
-	public void setFlagIamge() {
-		this.setIcon(scaledFlag);
-		this.isFlagged = true;
-	}
-	
-	public void eraseIcon() {
-		this.setIcon(null);
-		this.setText("");
-		this.isFlagged = false;
-	}
-	
-	public void revealBombsNear() {
-		this.setFont(new Font("Arial", Font.BOLD, 40));
-		this.setText("" + bombsNear);
-		this.setEnabled(false);
-
-	}
-	
-	public void eraseButton() {
-		this.isBomb = false;
-		this.bombsNear = 0;
-		this.eraseIcon();
-		this.setEnabled(true);
-	}
-	
-	
-	
-
 	private static final long serialVersionUID = 1L;
+
+	private static final int ICON_SIZE = 32;
+	private static final ImageIcon BOMB_ICON = loadScaledIcon("/images/bomb.jpg", ICON_SIZE, ICON_SIZE);
+	private static final ImageIcon FLAG_ICON = loadScaledIcon("/images/flag.png", ICON_SIZE, ICON_SIZE);
+
+	private final int row;
+	private final int col;
+
+	private boolean bomb;
+	private boolean flagged;
+	private boolean revealed;
+	private int bombsNear;
+
+	// Button Constructor.
+	public MineSweeperButton(int row, int col) {
+		this.row = row;
+		this.col = col;
+
+		setFont(new Font("Arial", Font.BOLD, 18));
+		setFocusPainted(false);
+	}
+
+	// Loads and scales and image icon from the given resource path.
+	private static ImageIcon loadScaledIcon(String resourcePath, int width, int height) {
+		URL url = MineSweeperButton.class.getResource(resourcePath);
+		if (url == null) {
+			return new ImageIcon();
+		}
+
+		ImageIcon icon = new ImageIcon(url);
+		Image scaled = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+		return new ImageIcon(scaled);
+
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public boolean isBomb() {
+		return bomb;
+	}
+
+	public boolean isFlagged() {
+		return flagged;
+	}
+
+	public boolean isRevealed() {
+		return revealed;
+	}
+
+	public void setBomb() {
+		this.bomb = true;
+	}
+
+	public int getBombsNear() {
+		return bombsNear;
+	}
+
+	public void setBombsNear(int bombsNear) {
+		this.bombsNear = bombsNear;
+	}
+
+	// Toggles flag.
+	public void toggleFlag() {
+		if (revealed) {
+			return;
+		}
+
+		if (flagged) {
+			clearDisplay();
+			flagged = false;
+		} else {
+			setIcon(FLAG_ICON);
+			setText("");
+			flagged = true;
+		}
+	}
+
+	public void revealBomb() {
+		setIcon(BOMB_ICON);
+		setText("");
+		revealed = true;
+		setEnabled(false);
+	}
+
+	public void revealNumber() {
+		clearDisplay();
+		setText(bombsNear > 0 ? String.valueOf(bombsNear) : "");
+		revealed = true;
+		setEnabled(false);
+	}
+
+	// Resets the button to its initial state.
+	public void resetButton() {
+		bomb = false;
+		flagged = false;
+		revealed = false;
+		bombsNear = 0;
+		clearDisplay();
+		setEnabled(true);
+	}
+
+	private void clearDisplay() {
+		setIcon(null);
+	}
 }
